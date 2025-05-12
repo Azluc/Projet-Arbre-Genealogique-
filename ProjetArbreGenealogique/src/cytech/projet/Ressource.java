ddb9bc1c8c77fbf430f582fe16305a5c79e1f55c
Mirzica--Vigé Lucas <106932876+Azluc@users.noreply.github.com>
2025-05-12 09:54:17 +0200

    Add files via upload
    Code Java

---
+++ ProjetArbreGenealogique/src/cytech/projet/Ressource.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Ressource {
+
+    /**
+     * Default constructor
+     */
+    public Ressource() {
+    }
+
+    /**
+     * 
+     */
+    private TypeDocument typeDocument;
+
+    /**
+     * 
+     */
+    public void contenu;
+
+    /**
+     * @param typeDocument 
+     * @param contenu
+     */
+    public void Ressource(TypeDocument typeDocument, void contenu) {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void partager() {
+        // TODO implement here
+    }
+
+}
