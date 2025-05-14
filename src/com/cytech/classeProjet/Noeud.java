cbd7a9eea827c8b7b428ae5731e6041e100c11a6
evanallouch <evanallouch@gmail.com>
2025-05-14 16:22:22 +0200

    Update Noeud.java
    Evan

--- src/com/cytech/classeProjet/Noeud.java
+++ src/com/cytech/classeProjet/Noeud.java
@@ -1,40 +1,56 @@
 package com.cytech.classeProjet;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * 
- */
 public class Noeud {
 
-    /**
-     * Default constructor
-     */
-    public Noeud() {
-    }
-
-    /**
-     * 
-     */
     private List<Personne> parent;
-
-    /**
-     * 
-     */
     private List<Personne> enfant;
 
-    /**
-     * @param personne
-     */
+    public Noeud() {
+        this.parent = new ArrayList<>();
+        this.enfant = new ArrayList<>();
+    }
+
     public void ajouterPersonne(Personne personne) {
-        // TODO implement here
+        if (personne != null) {
+            enfant.add(personne);
+            System.out.println(" l'enfant ajouté est : " + personne.getPrenom() + " " + personne.getNom());
+        }
     }
 
-    /**
-     * @param personne
-     */
-    public void modifierPersonne(Personne personne) {
-        // TODO implement here
+    public void modifierPersonne(Personne nouvellePersonne) {
+        boolean modifie = false;
+
+        for (int i = 0; i < parent.size(); i++) {
+            Personne p = parent.get(i);
+            if (p.getNom().equals(nouvellePersonne.getNom()) && p.getPrenom().equals(nouvellePersonne.getPrenom())) {
+                parent.set(i, nouvellePersonne);
+                modifie = true;
+                System.out.println(" Parent modifié : " + nouvellePersonne.getPrenom() + " " + nouvellePersonne.getNom());
+            }
+        }
+
+        for (int i = 0; i < enfant.size(); i++) {
+            Personne p = enfant.get(i);
+            if (p.getNom().equals(nouvellePersonne.getNom()) && p.getPrenom().equals(nouvellePersonne.getPrenom())) {
+                enfant.set(i, nouvellePersonne);
+                modifie = true;
+                System.out.println(" Enfant modifié : " + nouvellePersonne.getPrenom() + " " + nouvellePersonne.getNom());
+            }
+        }
+
+        if (!modifie) {
+            System.out.println(" Personne non trouvée dans ce nœud.");
+        }
     }
 
+    public List<Personne> getParents() {
+        return parent;
+    }
+
+    public List<Personne> getEnfants() {
+        return enfant;
+    }
 }
