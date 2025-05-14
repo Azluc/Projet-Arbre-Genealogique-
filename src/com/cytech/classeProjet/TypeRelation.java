fdd4787ebd056deac7a3a2e70d6e5ae2b0dfc9b7
Charf91 <chakroun480@gmail.com>
2025-05-14 21:05:37 +0200

    enumération type relation

--- src/com/cytech/classeProjet/TypeRelation.java
+++ src/com/cytech/classeProjet/TypeRelation.java
@@ -2,35 +2,10 @@ package com.cytech.classeProjet;
 
 import java.util.*;
 
-/**
- * 
- */
-public class TypeRelation {
-
-    /**
-     * Default constructor
-     */
-    public TypeRelation() {
-    }
-
-    /**
-     * 
-     */
-    public void ascendant;
-
-    /**
-     * 
-     */
-    public void descendant;
-
-    /**
-     * 
-     */
-    public void collateral;
-
-    /**
-     * 
-     */
-    public void union;
+public enum TypeRelation {
+ascendant,
+descendant,
+collateral,
+union
 
 }
