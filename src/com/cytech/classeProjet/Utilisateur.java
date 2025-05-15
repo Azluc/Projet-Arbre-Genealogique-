0d7484a886443c4c145a46b9188ae098cfda695c
evanallouch <evanallouch@gmail.com>
2025-05-15 12:35:06 +0200

    Update Utilisateur.java
    Evan

--- src/com/cytech/classeProjet/Utilisateur.java
+++ src/com/cytech/classeProjet/Utilisateur.java
@@ -3,6 +3,7 @@ package com.cytech.classeProjet;
 import java.util.Date;
 
 public class Utilisateur extends Personne {
+
     private Double codePublic;
     private Double codePrive;
     private String email;
@@ -10,12 +11,11 @@ public class Utilisateur extends Personne {
     private Double numeroSecuriteSociale;
     private String telephone;
     private String adresse;
-    
 
-    public Utilisateur(String nom, String prenom, String nationalite, Date dateNaissance, Date dateDeces,
+    public Utilisateur(int id, String nom, String prenom, String nationalite, Date dateNaissance, Date dateDeces,
                        Double codePublic, Double codePrive, String email, String motDePasse,
                        Double numeroSecuriteSociale, String telephone, String adresse) {
-        super(nom, prenom, nationalite, dateNaissance, dateDeces);
+        super(id, nom, prenom, nationalite, dateNaissance, dateDeces);
         this.codePublic = codePublic;
         this.codePrive = codePrive;
         this.email = email;
@@ -23,49 +23,21 @@ public class Utilisateur extends Personne {
         this.numeroSecuriteSociale = numeroSecuriteSociale;
         this.telephone = telephone;
         this.adresse = adresse;
-     
     }
 
-    public Double getCodePublic() {
-        return codePublic;
-    }
+    public Double getCodePublic() { return codePublic; }
+    public Double getCodePrive() { return codePrive; }
+    public String getEmail() { return email; }
+    public String getMotDePasse() { return motDePasse; }
+    public Double getNumeroSecuriteSociale() { return numeroSecuriteSociale; }
+    public String getTelephone() { return telephone; }
+    public String getAdresse() { return adresse; }
 
-    public Double getCodePrive() {
-        return codePrive;
-    }
-
-    public String getMotDePasse() {
-        return motDePasse;
-    }
-
-    public void setMotDePasse(String motDePasse) {
-        this.motDePasse = motDePasse;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Double getNumeroSecuriteSociale() {
-        return numeroSecuriteSociale;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone =telephone;
-    }
-
-    public String getAdresse() {
-        return adresse;
-    }
-
-    public void setAdresse(String adresse) {
-        this.adresse = adresse;
-    }}
+    public void setCodePublic(Double codePublic) { this.codePublic = codePublic; }
+    public void setCodePrive(Double codePrive) { this.codePrive = codePrive; }
+    public void setEmail(String email) { this.email = email; }
+    public void setMotDePasse(String motDePasse) { this.motDePasse = motDePasse; }
+    public void setNumeroSecuriteSociale(Double numeroSecuriteSociale) { this.numeroSecuriteSociale = numeroSecuriteSociale; }
+    public void setTelephone(String telephone) { this.telephone = telephone; }
+    public void setAdresse(String adresse) { this.adresse = adresse; }
+}
