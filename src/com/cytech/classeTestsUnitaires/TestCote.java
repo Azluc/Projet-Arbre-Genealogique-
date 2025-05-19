1c00458c79f6811bd6c26c8fdcc094c483722c2b
SdBlk95 <saidbelka29@gmail.com>
2025-05-19 16:37:22 +0200

    Create TestCote.java

---
+++ src/com/cytech/classeTestsUnitaires/TestCote.java
@@ -0,0 +1,19 @@
+package com.cytech.classeProjet;
+
+public class TestCote {
+    public static void main(String[] args) {
+        Cote c = Cote.MATERNEL;
+
+        if (c == Cote.MATERNEL) {
+            System.out.println("Test Cote MATERNEL : OK");
+        } else {
+            System.out.println("Test Cote MATERNEL : ÉCHEC");
+        }
+
+        if (Cote.NEUTRE.toString().equals("NEUTRE")) {
+            System.out.println("Test Cote NEUTRE : OK");
+        } else {
+            System.out.println("Test Cote NEUTRE : ÉCHEC");
+        }
+    }
+}
