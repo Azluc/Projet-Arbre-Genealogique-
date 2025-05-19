32f0e19d28044ef0d4bf21141611339f1c02c7e0
SdBlk95 <saidbelka29@gmail.com>
2025-05-19 15:17:45 +0200

    Update Ressource.java

--- src/com/cytech/classeProjet/Ressource.java
+++ src/com/cytech/classeProjet/Ressource.java
@@ -1,41 +1,46 @@
 package com.cytech.classeProjet;
 
-import java.util.*;
+import java.time.LocalDateTime;
 
-/**
- * 
- */
 public class Ressource {
 
-    /**
-     * Default constructor
-     */
-    public Ressource() {
-    }
-
-    /**
-     * 
-     */
     private TypeDocument typeDocument;
+    private String contenu; // peut être un lien vers une image, un texte, etc.
+    private LocalDateTime datePartage; // facultatif mais utile
 
-    /**
-     * 
-     */
-    public void contenu;
-
-    /**
-     * @param typeDocument 
-     * @param contenu
-     */
-    public void Ressource(TypeDocument typeDocument, void contenu) {
-        // TODO implement here
+    // Constructeur
+    public Ressource(TypeDocument typeDocument, String contenu) {
+        this.typeDocument = typeDocument;
+        this.contenu = contenu;
+        this.datePartage = LocalDateTime.now(); // Date automatique à la création
     }
 
-    /**
-     * 
-     */
+    // Méthode pour simuler le partage
     public void partager() {
-        // TODO implement here
+        System.out.println("Partage de " + typeDocument + " : " + contenu);
     }
 
+    // Getters
+    public TypeDocument getTypeDocument() {
+        return typeDocument;
+    }
+
+    public String getContenu() {
+        return contenu;
+    }
+
+    public LocalDateTime getDatePartage() {
+        return datePartage;
+    }
+
+    // Setters (facultatifs)
+    public void setContenu(String contenu) {
+        this.contenu = contenu;
+    }
+
+    // Affichage simple
+    @Override
+    public String toString() {
+        return "[" + typeDocument + "] " + contenu + " (partagé le " + datePartage + ")";
+    }
 }
