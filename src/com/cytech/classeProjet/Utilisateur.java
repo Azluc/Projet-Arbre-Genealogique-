e42ed55302f4e65ca5910bd0dc067cc2b9fc2a8b
SdBlk95 <saidbelka29@gmail.com>
2025-05-19 15:35:07 +0200

    Update Utilisateur.java

--- src/com/cytech/classeProjet/Utilisateur.java
+++ src/com/cytech/classeProjet/Utilisateur.java
@@ -1,97 +1,53 @@
 package com.cytech.classeProjet;
 
-import java.util.Date;
+public class Utilisateur {
 
-public class Utilisateur extends Personne {
-	
-	@SuppressWarnings("unused")
-	private String nom;
-	@SuppressWarnings("unused")
-	private String prenom;
-	@SuppressWarnings("unused")
-	private String nationalite;
-    private Double codePublic;
-    
-	private Double codePrive;
+    private String nom;
+    private String prenom;
     private String email;
     private String motDePasse;
-    private Double numeroSecuriteSociale;
-    private String telephone;
-    private String adresse;
-    @SuppressWarnings("unused")
-	private Date dateNaissance;
-    
-    
-	public Utilisateur(String nom, String prenom, String nationalite, Date dateNaissance, Date dateDeces,
-		    Double codePublic, Double codePrive, String email, String motDePasse,
-			Double numeroSecuriteSociale, String telephone, String adresse) {
-		super(nom, prenom, nationalite, dateNaissance, null);
-		this.codePublic = codePublic;
-		this.codePrive = codePrive;
-		this.email = email;
-		this.motDePasse = motDePasse;
-		this.numeroSecuriteSociale = numeroSecuriteSociale;
-		this.telephone = telephone;
-		this.adresse = adresse;
+    private boolean adhesionValidee;
 
-	}
-	public String getNationalite() {
-		return super.getNationalite();
-	}
-	public void setNationalite(String nationalite) {
-		super.setNationalite(nationalite);
-	}
-	public Double getCodePublic() {
-		return this.codePublic;
-	}
- 
-	public Double getCodePrive() {
-		return this.codePrive;
-	}
- 
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getMotDePasse() {
-		return motDePasse;
-	}
-	public void setMotDePasse(String motDePasse) {
-		this.motDePasse = motDePasse;
-	}
-	public Double getNumeroSecuriteSociale() {
-		return this.numeroSecuriteSociale;
-	}
- 
-	public String getTelephone() {
-		return this.telephone;
-	}
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-	public String getAdresse() {
-		return adresse;
-	}
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
-	public Date getDateNaissance() {
-		return super.getDateNaissance();
-	}
-	public void setDateNaissance(Date dateNaissance) {
-		super.setDateNaissance(dateNaissance);
-	}
-	public String getNom() {
-		return super.getNom();
-	}
-	public String getPrenom() {
-		return super.getPrenom();
-	}
-	 
-	 
- 
+    public Utilisateur(String nom, String prenom, String email, String motDePasse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.adhesionValidee = false; // par défaut
+    }
 
- 
+    // Getters
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public boolean isAdhesionValidee() {
+        return adhesionValidee;
+    }
+
+    // Setters
+    public void setAdhesionValidee(boolean adhesionValidee) {
+        this.adhesionValidee = adhesionValidee;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    @Override
+    public String toString() {
+        return prenom + " " + nom + " - " + email + (adhesionValidee ? " (validé)" : " (en attente)");
+    }
 }
