b5fe1f171b39a54c11b33c6573c052604728b790
SdBlk95 <saidbelka29@gmail.com>
2025-05-19 15:34:45 +0200

    Update Noeud.java

--- src/com/cytech/classeProjet/Noeud.java
+++ src/com/cytech/classeProjet/Noeud.java
@@ -3,13 +3,10 @@ package com.cytech.classeProjet;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Classe représentant un nœud dans l'arbre généalogique
- * avec une personne, ses parents et ses enfants.
- */
+
 public class Noeud {
 
-    private Personne personne; // personne centrale de ce nœud
+    private Personne personne;
     private List<Personne> parents;
     private List<Personne> enfants;
 
@@ -29,24 +26,12 @@ public class Noeud {
     public void ajouterParent(Personne parent) {
         if (parent != null && !parents.contains(parent)) {
             parents.add(parent);
-            System.out.println("Parent ajouté : " + parent.getPrenom() + " " + parent.getNom());
         }
     }
 
     public void ajouterEnfant(Personne enfant) {
         if (enfant != null && !enfants.contains(enfant)) {
             enfants.add(enfant);
-            System.out.println("Enfant ajouté : " + enfant.getPrenom() + " " + enfant.getNom());
-        }
-    }
-
-    public void modifierPersonne(Personne nouvellePersonne) {
-        if (personne != null && personne.getNom().equals(nouvellePersonne.getNom())
-                && personne.getPrenom().equals(nouvellePersonne.getPrenom())) {
-            this.personne = nouvellePersonne;
-            System.out.println("Personne du nœud modifiée : " + nouvellePersonne.getPrenom() + " " + nouvellePersonne.getNom());
-        } else {
-            System.out.println("Aucune correspondance dans ce nœud pour modifier.");
         }
     }
 
