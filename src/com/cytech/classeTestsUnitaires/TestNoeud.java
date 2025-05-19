0f6ac1189b16020ee42067904647212d5c3db3f0
SdBlk95 <saidbelka29@gmail.com>
2025-05-19 20:54:13 +0200

    Update TestNoeud.java

--- src/com/cytech/classeTestsUnitaires/TestNoeud.java
+++ src/com/cytech/classeTestsUnitaires/TestNoeud.java
@@ -5,14 +5,14 @@ import java.util.Date;
 public class TestNoeud_Console {
     public static void main(String[] args) {
 
-        // Création d’un nœud
+        
         Noeud noeud = new Noeud();
 
         Personne parent = new Personne("Durand", "Paul", "Française", new Date(), null);
         Personne enfant = new Personne("Durand", "Emma", "Française", new Date(), null);
         Personne centrale = new Personne("Durand", "Lucie", "Française", new Date(), null);
 
-        // Test 1 : setPersonne / getPersonne
+        
         noeud.setPersonne(centrale);
         if (noeud.getPersonne() == centrale) {
             System.out.println("Test 1 - set/getPersonne : OK");
@@ -20,7 +20,7 @@ public class TestNoeud_Console {
             System.out.println("Test 1 - set/getPersonne : ÉCHEC");
         }
 
-        // Test 2 : ajouterParent()
+       
         noeud.ajouterParent(parent);
         if (noeud.getParents().contains(parent)) {
             System.out.println("Test 2 - ajouterParent : OK");
@@ -28,7 +28,7 @@ public class TestNoeud_Console {
             System.out.println("Test 2 - ajouterParent : ÉCHEC");
         }
 
-        // Test 3 : ajouterEnfant()
+      
         noeud.ajouterEnfant(enfant);
         if (noeud.getEnfants().contains(enfant)) {
             System.out.println("Test 3 - ajouterEnfant : OK");
@@ -36,7 +36,7 @@ public class TestNoeud_Console {
             System.out.println("Test 3 - ajouterEnfant : ÉCHEC");
         }
 
-        // Test 4 : ajout double = pas de doublon
+     
         noeud.ajouterParent(parent);
         int nbParents = noeud.getParents().size();
         if (nbParents == 1) {
