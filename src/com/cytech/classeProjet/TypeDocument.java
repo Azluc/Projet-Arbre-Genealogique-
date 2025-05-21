39fabae83d9b09d7f2dc2af0a8a1f1d0f1e099f9
Charf91 <chakroun480@gmail.com>
2025-05-21 17:45:29 +0200

    Type document

--- src/com/cytech/classeProjet/TypeDocument.java
+++ src/com/cytech/classeProjet/TypeDocument.java
@@ -1,46 +1,9 @@
 package com.cytech.classeProjet;
 
-import java.time.LocalDateTime;
-
-public class TypeDocument {
-
-    private TypeDocument typeDocument;
-    private String contenu; // peut être un lien vers une image, un texte, etc.
-    private LocalDateTime datePartage; // facultatif mais utile
-
-    // Constructeur
-    public TypeDocument(TypeDocument typeDocument, String contenu) {
-        this.typeDocument = typeDocument;
-        this.contenu = contenu;
-        this.datePartage = LocalDateTime.now(); // Date automatique à la création
-    }
-
-    // Méthode pour simuler le partage
-    public void partager() {
-        System.out.println("Partage de " + typeDocument + " : " + contenu);
-    }
-
-    // Getters
-    public TypeDocument getTypeDocument() {
-        return typeDocument;
-    }
-
-    public String getContenu() {
-        return contenu;
-    }
-
-    public LocalDateTime getDatePartage() {
-        return datePartage;
-    }
-
-    // Setters (facultatifs)
-    public void setContenu(String contenu) {
-        this.contenu = contenu;
-    }
-
-    // Affichage simple
-    @Override
-    public String toString() {
-        return "[" + typeDocument + "] " + contenu + " (partagé le " + datePartage + ")";
-    }
+public enum TypeDocument {
+    TEXTE,
+    IMAGE,
+    VIDEO,
+    LIEN,
+    PDF
 }
